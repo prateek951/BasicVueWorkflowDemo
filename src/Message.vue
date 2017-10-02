7870704e6d93533d69e6f9fce626b21b446efcbe
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2017-10-03 01:09:55 +0530

    Add files via upload

---
+++ src/Message.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h1>{{message}}</h1>
+		<app-input :msg = "message" @messageChanged="message = $event"></app-input>
+	</div>
+</template>
+
+<script>
+	import input from './input'
+	export default {
+		data() {
+			return {
+				message: 'This is a great message'
+			}
+		},
+		components: {
+			'app-input' : input
+		}
+	}
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Message from  './Message'
+
+//MY COMPONENT GOT REGISTERED
+Vue.component('app-message',Message);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
